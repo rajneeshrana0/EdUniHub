8ba7181b7e7bbc149c327415b78883ef1a3e8e67
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2023-11-25 22:58:36 +0530

    readme update

---
+++ controllers/Course.js
@@ -0,0 +1,25 @@
+const Course  = require("../models/Course");
+const Tag  = require("../models/tags");
+const User = require("../models/User");
+const {uploadImageToCloudinary} = require("../utils/imageUploader");
+
+
+// create Course handler Function 
+
+exports.createCourse = async (req,res) =>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+// getAll Courses handler Function
