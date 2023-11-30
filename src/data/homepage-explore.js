671275030b024897cd4f4e69fce80fc50bffa0f7
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2023-11-30 14:05:59 +0530

    Update homepage-explore.js

--- src/data/homepage-explore.js
+++ src/data/homepage-explore.js
@@ -1,6 +1,6 @@
 export const HomePageExplore = [
     {
-        tag: 'Free',
+        tag: 'Btech',
         courses : [
             {
                 heading : "Btech",
@@ -23,7 +23,7 @@ export const HomePageExplore = [
         ]
     },
     {
-        tag: 'New to coding',
+        tag: 'BCA',
         courses : [
             {
                 heading : "HTML",
@@ -46,7 +46,7 @@ export const HomePageExplore = [
         ]
     },
     {
-        tag: 'Most popular',
+        tag: 'BBA',
         courses : [
             {
                 heading : "Java",
@@ -69,7 +69,7 @@ export const HomePageExplore = [
         ]
     },
     {
-        tag: 'Skills paths',
+        tag: 'MBA',
         courses : [
             {
                 heading : "Flask",
@@ -92,7 +92,7 @@ export const HomePageExplore = [
         ]
     },
     {
-        tag: 'Career paths',
+        tag: 'MCA',
         courses : [
             {
                 heading : "Next.js",
