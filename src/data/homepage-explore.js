e6c431f73aa5200b2230f3754b525bbced5fe673
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2023-11-30 14:11:41 +0530

    Update homepage-explore.js

--- src/data/homepage-explore.js
+++ src/data/homepage-explore.js
@@ -3,19 +3,19 @@ export const HomePageExplore = [
         tag: 'Btech',
         courses : [
             {
-                heading : "Btech",
+                heading : "Btech 1st Sem",
                 description : "BTech stands for Bachelor of Technology. It is an undergraduate four-year course and is offered in various disciplines and specialisations.",
                 level : 'Beginner',
                 lessionNumber : 5
             },
             {
-                heading : "BCA",
+                heading : "Btech 2nd Sem",
                 description : "The course is designed to provide students with the knowledge and skills required to succeed in computer science and information technology.",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
-                heading : "BBA",
+                heading : "Btech 3rd Sem",
                 description : "A Bachelor of Business Administration (BBA) is a bachelor's degree that helps you establish a fundamental understanding of business and how various aspects of it apply to the real world",
                 level : 'Beginner',
                 lessionNumber : 6
@@ -26,20 +26,20 @@ export const HomePageExplore = [
         tag: 'BCA',
         courses : [
             {
-                heading : "HTML",
-                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                heading : "BCA 1st Sem",
+                description : "The course is designed to provide students with the knowledge and skills required to succeed in computer science and information technology.",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
-                heading : "CSS",
-                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                heading : "BCA 2nd Sem",
+                description : "The course is designed to provide students with the knowledge and skills required to succeed in computer science and information technology.",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
-                heading : "Responsive ",
-                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                heading : "BCA 3rd Sem ",
+                description : "The course is designed to provide students with the knowledge and skills required to succeed in computer science and information technology.",
                 level : 'Beginner',
                 lessionNumber : 6
             },
@@ -49,20 +49,20 @@ export const HomePageExplore = [
         tag: 'BBA',
         courses : [
             {
-                heading : "Java",
-                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                heading : "BBA 1st Sem",
+                description : "A Bachelor of Business Administration (BBA) is a bachelor's degree that helps you establish a fundamental understanding of business and how various aspects of it apply to the real world",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
-                heading : "Python",
-                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
+                heading : "BBA 2nd Sem",
+                description : "A Bachelor of Business Administration (BBA) is a bachelor's degree that helps you establish a fundamental understanding of business and how various aspects of it apply to the real world",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
-                heading : "SCSS",
-                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                heading : "BBA 3rd Sem",
+                description : "A Bachelor of Business Administration (BBA) is a bachelor's degree that helps you establish a fundamental understanding of business and how various aspects of it apply to the real world",
                 level : 'Beginner',
                 lessionNumber : 6
             },
@@ -82,7 +82,7 @@ export const HomePageExplore = [
                 description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
                 level : 'Beginner',
                 lessionNumber : 6
-            },
+            },  
             {
                 heading : "Fast API",
                 description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
