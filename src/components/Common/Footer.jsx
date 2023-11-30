178459079d05427127805815e8b7130410ca1df0
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2023-11-30 14:26:41 +0530

    Update Footer.jsx

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -10,14 +10,12 @@ import { FaFacebook, FaGoogle, FaTwitter, FaYoutube } from "react-icons/fa";
 
 const BottomFooter = ["Privacy Policy", "Cookie Policy", "Terms"];
 const Resources = [
-  "Articles",
+  
   "Blog",
-  "Chart Sheet",
-  "Code challenges",
+  
   "Docs",
   "Projects",
-  "Videos",
-  "Workspaces",
+  
 ];
 const Plans = ["Paid memberships", "For students", "Business solutions"];
 const Community = ["Forums", "Chapters", "Events"];
