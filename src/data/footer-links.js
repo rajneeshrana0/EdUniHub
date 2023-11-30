3638e94bc4d99ab9f8673bb4d13a93578f5e8008
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2023-11-30 09:39:19 +0530

    Update footer-links.js

--- src/data/footer-links.js
+++ src/data/footer-links.js
@@ -22,34 +22,22 @@ export const FooterLink2 = [
     ],
   },
   {
-    title: "Languages",
+    title: "Branches",
     links: [
-      { title: "Bash", link: "/bash" },
-      { title: "C++", link: "/c++" },
-      { title: "C#", link: "/csharp" },
-      { title: "Go", link: "/go" },
-      { title: "HTML & CSS", link: "/html-css" },
-      { title: "Java", link: "/java" },
-      { title: "JavaScript", link: "/javascript" },
-      { title: "Kotlin", link: "/kotlin" },
-      { title: "PHP", link: "/php" },
-      { title: "Python", link: "/python" },
-      { title: "R", link: "/r" },
-      { title: "Ruby", link: "/ruby" },
-      { title: "SQL", link: "/sql" },
-      { title: "Swift", link: "/swift" },
+      { title: "BTECH", link: "/btech" },
+      { title: "BCA", link: "/bca" },
+      { title: "BBA", link: "/bba" },
+      { title: "MBA", link: "/mba" },
+      { title: "PHARMACY", link: "/pharmacy" },
+      
     ],
   },
   {
     title: "Career building",
     links: [
         {title: "Career paths", link: "/career-paths"},
-        {title: "Career services", link: "/career-services"},
         {title: "Interview prep", link: "/interview-prep"},
-        {title: "Professional certification", link: "/professional-certification"},
-        {title: "-", link: "/hi"},
-        {title: "Full Catalog", link: "/full-catalog"},
-        {title: "Beta Content", link: "/beta-content"}
+        
     ]
   }
 ];
