eabc29b80453a0a6e52bbfff90be38b5ac57ab63
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2023-11-30 13:58:40 +0530

    Update homepage-explore.js

--- src/data/homepage-explore.js
+++ src/data/homepage-explore.js
@@ -3,20 +3,20 @@ export const HomePageExplore = [
         tag: 'Free',
         courses : [
             {
-                heading : "Learn HTML",
-                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                heading : "Btech",
+                description : "BTech stands for Bachelor of Technology. It is an undergraduate four-year course and is offered in various disciplines and specialisations.",
+                level : 'Beginner',
+                lessionNumber : 5
+            },
+            {
+                heading : "BCA",
+                description : "The course is designed to provide students with the knowledge and skills required to succeed in computer science and information technology.",
                 level : 'Beginner',
                 lessionNumber : 6
             },
             {
-                heading : "Learn CSS",
-                description : "This course explores advanced topics in HTML5 and CSS3, including animations, transitions, and layout techniques",
-                level : 'Beginner',
-                lessionNumber : 6
-            },
-            {
-                heading : "Responsive Web design",
-                description : "This course teaches responsive web design techniques, allowing web pages to adapt to different devices and screen sizes",
+                heading : "BBA",
+                description : "A Bachelor of Business Administration (BBA) is a bachelor's degree that helps you establish a fundamental understanding of business and how various aspects of it apply to the real world",
                 level : 'Beginner',
                 lessionNumber : 6
             },
