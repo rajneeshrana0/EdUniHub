03b614b667606eb1498594c31ab867e776814fcb
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2023-12-02 21:06:30 +0530

    index for server updated

--- server/index.js
+++ server/index.js
@@ -1,66 +1,58 @@
-// Importing necessary modules and packages
 const express = require("express");
 const app = express();
-// const userRoutes = require("./routes/user");
-const userRoutes = require("./routes/user");
-const profileRoutes = require("./routes/profile");
-const courseRoutes = require("./routes/Course");
+
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
 const paymentRoutes = require("./routes/Payments");
+const courseRoutes = require("./routes/Course");
 const contactUsRoute = require("./routes/Contact");
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const { cloudinaryConnect } = require("./config/cloudinary");
+const {cloudinaryConnect } = require("./config/cloudinary");
 const fileUpload = require("express-fileupload");
 const dotenv = require("dotenv");
 
-// Setting up port number
+dotenv.config();
 const PORT = process.env.PORT || 4000;
 
-// Loading environment variables from .env file
-dotenv.config();
-
-// Connecting to database
+//database connect
 database.connect();
- 
-// Middlewares
+//middlewares
 app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin: "*",
-		credentials: true,
+		origin:"http://localhost:3000",
+		credentials:true,
 	})
-);
+)
+
 app.use(
 	fileUpload({
-		useTempFiles: true,
-		tempFileDir: "/tmp/",
+		useTempFiles:true,
+		tempFileDir:"/tmp",
 	})
-);
-
-// Connecting to cloudinary
+)
+//cloudinary connection
 cloudinaryConnect();
 
-// Setting up routes
-app.use("/api/v1/user", userRoutes);
+//routes
+app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/reach", contactUsRoute);
-// app.use("/api/v1/user", userRoutes);
 
-// Testing the server
+//def route
+
 app.get("/", (req, res) => {
 	return res.json({
-		success: true,
-		message: "Your server is up and running ...",
+		success:true,
+		message:'Your server is up and running....'
 	});
 });
 
-// Listening to the server
 app.listen(PORT, () => {
-	console.log(`App is listening at ${PORT}`);
-});
-
-// End of code.
+	console.log(`App is running at ${PORT}`)
+})
