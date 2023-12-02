c8ec19020dac3bf270f694f1edd6f1438c1cf940
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2023-12-02 21:48:41 +0530

    1

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 // Importing necessary modules and packages
 const express = require("express");
 const app = express();
+// const userRoutes = require("./routes/user");
 const userRoutes = require("./routes/user");
+
 const profileRoutes = require("./routes/profile");
 const courseRoutes = require("./routes/Course");
 const paymentRoutes = require("./routes/Payments");
