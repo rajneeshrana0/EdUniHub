d1303af06b86fbbf43b0a8c1b310e2192fc582c5
Rajneesh rana <90295886+rajneeshrana0@users.noreply.github.com>
2023-12-02 22:53:53 +0530

    56

--- server/index.js
+++ server/index.js
@@ -2,53 +2,53 @@
 const express = require("express");
 const app = express();
 
-const userRoutes = require("./routes/user");
+// const userRoutes = require("./routes/user");
 
-const profileRoutes = require("./routes/profile");
-const courseRoutes = require("./routes/Course");
-const paymentRoutes = require("./routes/Payments");
-const contactUsRoute = require("./routes/Contact");
-const database = require("./config/database");
-const cookieParser = require("cookie-parser");
-const cors = require("cors");
-const { cloudinaryConnect } = require("./config/cloudinary");
-const fileUpload = require("express-fileupload");
-const dotenv = require("dotenv");
+// const profileRoutes = require("./routes/profile");
+// const courseRoutes = require("./routes/Course");
+// const paymentRoutes = require("./routes/Payments");
+// const contactUsRoute = require("./routes/Contact");
+// const database = require("./config/database");
+// const cookieParser = require("cookie-parser");
+// const cors = require("cors");
+// const { cloudinaryConnect } = require("./config/cloudinary");
+// const fileUpload = require("express-fileupload");
+// const dotenv = require("dotenv");
 
 // Setting up port number
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 4000;
 
 // Loading environment variables from .env file
-dotenv.config();
+// dotenv.config();
 
 // Connecting to database
-database.connect();
+// database.connect();
  
 // Middlewares
 app.use(express.json());
-app.use(cookieParser());
-app.use(
-	cors({
-		origin: "*",
-		credentials: true,
-	})
-);
-app.use(
-	fileUpload({
-		useTempFiles: true,
-		tempFileDir: "/tmp/",
-	})
-);
+// app.use(cookieParser());
+// app.use(
+// 	cors({
+// 		origin: "*",
+// 		credentials: true,
+// 	})
+// );
+// app.use(
+// 	fileUpload({
+// 		useTempFiles: true,
+// 		tempFileDir: "/tmp/",
+// 	})
+// );
 
 // Connecting to cloudinary
-cloudinaryConnect();
+// cloudinaryConnect();
 
 // Setting up routes
-app.use("/api/v1/auth", userRoutes);
-app.use("/api/v1/profile", profileRoutes);
-app.use("/api/v1/course", courseRoutes);
-app.use("/api/v1/payment", paymentRoutes);
-app.use("/api/v1/reach", contactUsRoute);
+// app.use("/api/v1/auth", userRoutes);
+// app.use("/api/v1/profile", profileRoutes);
+// app.use("/api/v1/course", courseRoutes);
+// app.use("/api/v1/payment", paymentRoutes);
+// app.use("/api/v1/reach", contactUsRoute);
 
 // Testing the server
 app.get("/", (req, res) => {
